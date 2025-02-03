0493d16e47363ae3a73dd9d36468c9b05141fb3f
rootziro <sploit55@gmail.com>
2025-02-03 02:57:08 +0000

    ready for debug

--- BHP/Tcp-sever.py
+++ BHP/Tcp-sever.py
@@ -12,3 +12,18 @@ def main():
     server.bind((IP, PORT))
     server.listen(5)
     print(f'[*] Listening on {IP}:{PORT}')
+
+    while True:
+        client, addr = server.accept()
+        print(f'[*] Accepted connection from: {addr[0]}:{addr[1]}')
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
+
+        def handle_client(client_socket):
+            with client_socket as sock:
+                request = sock.recv(1024)
+                print(f'[*] Received: {request.decode("utf-8")}')
+                sock.send(b'ACK')
+
+if __name__ == '__main__':
+    main()
